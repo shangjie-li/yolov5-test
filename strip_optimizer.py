25a6cc4d811b9f0ae9662620bf8585abf08ffea1
shangjie-li <1814481684@qq.com>
2022-05-09 10:54:55 +0800

    Update strip_optimizer.py

--- strip_optimizer.py
+++ strip_optimizer.py
@@ -5,17 +5,16 @@ except ImportError:
     sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
     import cv2
 
-from utils.general import *
+from utils.general import strip_optimizer
+
 
 if __name__ == '__main__':
+    # Usage: python strip_optimizer.py path_to_your_ckpt
     if len(sys.argv) == 2:
-        # Usage: python dataset_player.py weights/yolov5s_coco_100.pt
         weights = sys.argv[1]
         print('Stripping optimizer in weights: %s...' % weights)
-        print()
     else:
-        print('Error: Only one parameter (weights) is supported.')
+        print('Error: Only the parameter path_to_your_ckpt is needed.')
         exit()
-    
     strip_optimizer(weights)
 
